ef6a33cff292b71ef22f19c581457e1a073842d2
Jared Malone <jaredmm@uw.edu>
2019-03-06 08:34:31 -0800

    refactored with better CORS

--- functions/index.js
+++ functions/index.js
@@ -1,7 +1,6 @@
-// used to parse HTML POST requests
 const escapeHtml = require('escape-html');
 
-// The Firebase Functions and Admin SDK
+// Get db object from Cloud Firestore
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
@@ -9,52 +8,72 @@ admin.initializeApp();
 const db = admin.firestore();
 
 // App configuration
+const CORS_DOMAIN = '*';
 const COLLECTION_NAME = '/scores';
 const MAX_RANK = 100;
 
 /**
- * Check if a score qualifies where rank <= MAX_RANK
+ * Check if a score qualifies to be on the leaderboard.
+ * A score qualifies where rank <= MAX_RANK.
  */
 exports.checkScore = functions.https.onRequest((req, res) => {
-  // validate request type
-  if (req.method !== 'GET') {
+  // set CORS headers
+  res.set('Access-Control-Allow-Origin', CORS_DOMAIN);
+
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else if (req.method === 'GET') {
+  
+    // store parameters
+    let data = req.query || {};
+    let theScore = Number.parseInt(escapeHtml(data.score));
+    
+    if (theScore) {
+      getAllScores().then(data => {
+        let result;
+        let rank;
+
+        for (let e of data) {
+          if (e.score <= theScore) {
+            result = e.rank;
+            break;
+          } else {
+            rank = e.rank + 1;
+          }
+        }
+
+        if (!result) {
+          result = rank;
+        }
+
+        if (result > MAX_RANK) {
+          result = false;
+        }
+        
+        return res.send({ rank: result });
+      }).catch(error => {
+        // db access error
+        console.log({
+          error: 'Cloud Firestore access error',
+          function: 'checkScore',
+          time: Date.UTC(),
+          msg: error
+        })
+        return res.status(500).end();
+      });
+
+    } else {
+      // handle missing parameters
+      return res.status(400).end();
+    }
+  } else {
+    // handle requests other than GET
     return res.status(405).end();
   }
-
-  res.set('Access-Control-Allow-Origin', '*');
-
-  // store parameters
-  let data = req.query || {};
-  let score = Number.parseInt(escapeHtml(data.score));
-  
-  if (score) {
-    getAllScores().then(data => {
-      let result;
-      let rank;
-
-      for (let e of data) {
-        if (e.score <= score) {
-          result = e.rank;
-          break;
-        } else {
-          rank = e.rank + 1;
-        }
-      }
-
-      if (!result) {
-        result = rank;
-      }
-      
-      return res.send({ rank: result });
-    }).catch(error => {
-      console.log(error);
-      return res.status(500).end();
-    });
-
-  } else {
-    return res.status(400).end();
-  }
-});  
+});
 
 
 /**
@@ -63,87 +82,121 @@ exports.checkScore = functions.https.onRequest((req, res) => {
  * post-condition:  name and score are stored in the db
  */
 exports.postScore = functions.https.onRequest((req, res) => {
-  // validate request type
-  if (req.method !== 'POST') {
-    return res.status(405).end();
-  }
+  // set CORS headers
+  res.set('Access-Control-Allow-Origin', CORS_DOMAIN);
 
-  res.set('Access-Control-Allow-Origin', '*');
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else if (req.method === 'POST') {
 
-  // store parameters
-  let data = req.body || {};
-  let name = escapeHtml(data.name);
-  let score = Number.parseInt(escapeHtml(data.score));
+    // store parameters
+    let data = req.body || {};
+    let name = escapeHtml(data.name);
+    let score = Number.parseInt(escapeHtml(data.score));
 
-  if (name && score) {
+    if (name && score) {
 
-    // check for existing record to update
-    db.collection(COLLECTION_NAME).where('name', '==', name).get()
-    .then(snapshot => {
+      // check for existing record to update
+      db.collection(COLLECTION_NAME).where('name', '==', name).get()
+      .then(snapshot => {
 
-      if (snapshot.empty) {
-        db.collection(COLLECTION_NAME).add({
-          name,
-          score
-        })
-        .then(() => {
-          return res.send({ result: 'success' });
-        }).catch(err => {
-          return res.status(500).end();
-        });
-      } else {
-        let index = snapshot.docs.indexOf(0).id;
+        if (snapshot.empty) {
+          db.collection(COLLECTION_NAME).add({
+            name,
+            score
+          })
+          .then(() => {
+            return res.send({ result: 'success' });
+          }).catch(err => {
+            return res.status(500).end();
+          });
+        } else {
+          let index = snapshot.docs.indexOf(0).id;
 
-        db.collection(COLLECTION_NAME).doc(index).update(score)
-        .then(() => {
-          return res.send({ result: 'success' });
-        }).catch(err => {
-          return res.status(500).end();
-        });
-      }
-    });
+          db.collection(COLLECTION_NAME).doc(index).update(score)
+          .then(() => {
+            return res.send({ result: 'success' });
+          }).catch(error => {
+            // db access error
+            console.log({
+              error: 'Cloud Firestore access error',
+              function: 'postScore',
+              time: Date.UTC(),
+              msg: error
+            })
+            return res.status(500).end();
+          });
+        }
+      });
+
+    } else {
+      // handle missing parameters
+      return res.status(400).end();
+    }
   } else {
-    return res.status(400).end();
+      // handle requests other than POST
+      return res.status(405).end();
   }
 });
 
 
 // function to get scores
 exports.getScores = functions.https.onRequest((req, res) => {
-  // validate request type
-  if (req.method !== 'GET') {
-    return res.status(405).end();
-  }
+  // set CORS headers
+  res.set('Access-Control-Allow-Origin', CORS_DOMAIN);
 
-  res.set('Access-Control-Allow-Origin', '*');
-
-  // store parameters
-  let data = req.query || {};
-  let name = escapeHtml(data.name);
-
-  let result = {};
-  result.userRank = {};
+  if (req.method === 'OPTIONS') {
+    res.set('Access-Control-Allow-Methods', 'GET');
+    res.set('Access-Control-Allow-Headers', 'Content-Type');
+    res.set('Access-Control-Max-Age', '3600');
+    res.status(204).send('');
+  } else if (req.method === 'GET') {  
   
-  getAllScores().then(data => {
-    result.leaderboard = data || new Array();
+    // store parameters
+    let data = req.query || {};
+    let name = escapeHtml(data.name);
 
-    if (name) {
-      // check for name within data
-      for (let e of result.leaderboard) {
-        if (e.name === name) {
-          result.userRank.rank = e.rank;
-          result.userRank.score = e.score;
+    let result = {};
+    result.userRank = {};
+    
+    getAllScores().then(data => {
+      result.leaderboard = data || new Array();
+
+      if (name) {
+        // check for name within data
+        for (let e of result.leaderboard) {
+          if (e.name === name) {
+            result.userRank.rank = e.rank;
+            result.userRank.score = e.score;
+          }
         }
       }
-    }
 
-    return res.send(result);
-  }).catch(error => {
-    console.log(error);
-    return res.status(500).end();
-  });
+      return res.send(result);
+    }).catch(error => {
+      // db access error
+      console.log({
+        error: 'Cloud Firestore access error',
+        function: 'getScores',
+        time: Date.UTC(),
+        msg: error
+      })
+      return res.status(500).end();
+    });
+
+  } else {
+    // handle requests other than GET
+    return res.status(405).end();
+  }
 });
 
+
+/**
+ * Get all scores from db and assign rank
+ */
 function getAllScores() {
   return db.collection(COLLECTION_NAME).orderBy('score', 'desc').limit(MAX_RANK).get()
   .then(snapshot => {
